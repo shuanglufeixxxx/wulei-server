59e7354cf354b30e478718d94332c20628b4f84b
Lufei Shuang <shuanglufei1994@gmail.com>
2021-02-15 21:52:01 +0700

    remove wrong post api

--- src/main/java/wulei/controller/PostController.java
+++ src/main/java/wulei/controller/PostController.java
@@ -6,9 +6,6 @@ import wulei.modelpublic.PostPublic;
 import wulei.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.Arrays;
-import java.util.List;
-
 @RestController
 @RequestMapping("/post")
 class PostController {
@@ -19,14 +16,5 @@ class PostController {
     @GetMapping("/{id}")
     public PostPublic get(@PathVariable long id) {
         return new PostPublic( this.postRepository.findById(id).get() );
-    }
-
-    @GetMapping
-    public List<PostPublic> getByClassify(@RequestParam String classify) {
-        return Arrays.asList(postRepository.findByClassifyFeatured(classify)
-                .stream()
-                .map(PostPublic::new)
-                .toArray(PostPublic[]::new)
-        );
     }
 }
